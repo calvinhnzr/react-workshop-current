2b9e67b0e4bd6c6acb55dd2a12c09a0fa638c84c
calvinhnzr <calvinhinzer@gmail.com>
2024-05-10 14:35:01 +0200

    [FEATURE] useEffect example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import Header from "./components/Header"
 import { Footer } from "./components/Footer"
+import Button from "./components/Button"
 
 const Main = (props) => {
+  useEffect(() => {
+    console.log("Component mounted")
+
+    return () => {
+      console.log("Component unmounted")
+    }
+  }, [])
+
   return (
     <main>
       <h1>{props.title}</h1>
@@ -12,27 +21,28 @@ const Main = (props) => {
   )
 }
 
-function Button(props) {
-  return <button onClick={props.handleClick}>{props.count}</button>
-}
-
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [boolean, setBoolean] = useState(false)
+
+  // useEffect(() => {
+  //   console.log("Component mounted")
+  //   //
+  //   return () => {
+  //     // cleanup
+  //   }
+  // }, [])
 
   function handleClick() {
-    setCount(count + 1)
+    setBoolean(!boolean)
   }
 
   return (
     <div>
       <Header />
-      <Button handleClick={handleClick} count={count} />
-      <button onClick={() => setCount(count + 1)}>Click</button>
+      <Button handleClick={handleClick} />
 
-      <p>{count}</p>
-      <Main title="Hello World!" color="blue">
-        <p>Some content</p>
-      </Main>
+      {boolean ? <Main title="Hello World!" color="blue" /> : ""}
+
       <Footer />
     </div>
   )
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+function Button(props) {
+  return <button onClick={props.handleClick}>Click Me</button>
+}
+
+export default Button
